87ac55ef0fb9a1abf7502be7e0ad88ba3c4723d3
kkh12345 <https://github.com/kkh12345/kkh2.git>
2025-05-30 04:31:42 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,152 @@
+//전역변수
+const checkboxList = document.querySelectorAll(
+  '.filter-container__check-item>input[type=checkbox]'
+);
+
+let projectsArray = [];
+
+//프로젝트 데이터 받아오기
+async function loadProjects() {
+  projectsArray = [];
+
+  try {
+    const response = await fetch('./projects.json');
+    const data = await response.json();
+    projectsArray = [...data];
+  } catch (err) {
+    alert(`에러 : ${err}`);
+  }
+
+  //로딩중일 땐 메세지 띄우기
+  createLoadingMessage();
+  //로딩 완료되면 카드 만들고 로딩 완료 메세지로 바꾼 후 없애기
+  if (projectsArray.length > 0) {
+    filterProjects();
+    removeLoadingMessage();
+  }
+}
+
+//로딩 메세지 생성
+function createLoadingMessage() {
+  let template = `<p class="loading-message">포트폴리오 로딩중입니다...</p>`;
+  document.body.insertAdjacentHTML('beforeend', template);
+}
+
+//로딩 완료 메세지 없애기
+function removeLoadingMessage() {
+  const loadingMessage = document.querySelector('.loading-message');
+  setTimeout(() => {
+    loadingMessage.textContent = '포트폴리오 로딩이 완료되었습니다.';
+    setTimeout(() => {
+      loadingMessage.remove();
+    }, 500);
+  }, 50);
+}
+
+//필터링 후 카드 생성
+function filterProjects() {
+  const skillCheckboxList = document.querySelectorAll(
+    '.filter-container__left input[type=checkbox]'
+  );
+  const optionCheckboxList = document.querySelectorAll(
+    '.filter-container__right input[type=checkbox]'
+  );
+  const optionCheckValueArray = [];
+  const skillCheckValueArray = [];
+
+  let filterArray = [...projectsArray];
+
+  //모든 체크박스의 id륿 배열에 담음
+  [...optionCheckboxList].forEach((a) => {
+    if (a.disabled === false && a.checked) {
+      optionCheckValueArray.push(a.id);
+    }
+  });
+  [...skillCheckboxList].forEach((a) => {
+    if (a.checked) {
+      skillCheckValueArray.push(a.id);
+    }
+  });
+
+  //필터링
+  for (let value of optionCheckValueArray) {
+    if (value === 'key-projects') {
+      filterArray = filterArray.filter(
+        (project) => project.isKeyProject === true
+      );
+    } else if (value === 'all-projects') {
+      filterArray = [...projectsArray];
+    }
+  }
+
+  for (let value of skillCheckValueArray) {
+    filterArray = filterArray.filter(
+      (project) => project.skills.some((skill) => skill === value) === true
+    );
+  }
+
+  createProjectCard(filterArray);
+}
+
+//카드 생성해주는 함수
+function createProjectCard(arr) {
+  const nullMessage = document.querySelector(
+    '.project-container__null-message '
+  );
+  const projectList = document.querySelector('.project-container__list');
+
+  [...projectList.children].forEach((a) => a.remove());
+
+  if (arr.length > 0) {
+    let template = '';
+    nullMessage.style.display = 'none';
+
+    arr.forEach((a) => {
+      const workers = a.workers > 1 ? 'Team' : 'Single';
+      template += ` <li class="project-container__item">
+              <div class="project-card">
+                <a href="#">
+                  <div class="project-card__thumb-wrap">
+                    <img
+                      class="project-card__thumb"
+                      src=${a.img}
+                      alt=${a.title}
+                    />
+                  </div>
+                  <div class="project-card__txt-wrap">
+                    <h3 class="project-card__tit">${a.title}</h3>
+                    <span class="project-card__badge ${workers}">${workers}</span>
+                  </div>
+                </a>
+              </div>
+            </li>`;
+    });
+
+    projectList.insertAdjacentHTML('beforeend', template);
+  } else {
+    nullMessage.style.display = 'block'; //일치하는 결과가 없을 때 띄우는 메세지
+  }
+}
+
+//필터 체크박스 클릭했을 때 실행
+checkboxList.forEach((checkbox) => {
+  checkbox.addEventListener('change', checkBoxChangeFunction);
+});
+
+function checkBoxChangeFunction(e) {
+  const allProjects = document.getElementById('all-projects');
+  const keyProjects = document.getElementById('key-projects');
+
+  if (e.target.id === 'all-projects') {
+    keyProjects.checked = false;
+    allProjects.checked = true;
+  } else if (e.target.id === 'key-projects') {
+    allProjects.checked = false;
+    keyProjects.checked = true;
+  }
+
+  filterProjects();
+}
+
+//함수 실행
+loadProjects();
